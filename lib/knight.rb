3ff52ccbc9df512db26a2a6971e01a4a019f0bbc
parentda <danielparent21@gmail.com>
2021-09-15 14:44:59 -0400

    completed Knight class

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class Knight
+  @@movement = [2, 1, -1, -2]
+
+  def self.moves
+    @@movement.permutation(2).filter { |pair| pair[0].abs != pair[1].abs }
+  end
 end
