301118119870cb6f48e1422212ae425854229a3b
parentda <danielparent21@gmail.com>
2021-09-15 14:43:14 -0400

    added initialize and insert methods to Tree class

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class Tree
+  attr_accessor :root
+
+  def initialize(starting_coords)
+    @root = insert(nil, starting_coords)
+  end
+
+  def insert(parent_node, coords)
+    node = Node.new(coords)
+    parent_node.nil? ? node : parent_node.next_moves << node
+  end
 end
