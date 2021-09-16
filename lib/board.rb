849552fdb817793fe090864ecf783f2f5f4ec6e7
parentda <danielparent21@gmail.com>
2021-09-15 21:07:29 -0400

    added display method for debugging purposes

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,8 @@ class Board
   def initialize
     @positions = Array.new(8) { Array.new(8, 0) }
   end
+
+  def display
+    @positions.each_with_index { |row, index| puts "#{index}: #{row}" }
+  end
 end
