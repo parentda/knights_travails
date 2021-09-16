989f394fa197c5db32734323fe6299e418643a61
parentda <danielparent21@gmail.com>
2021-09-15 22:07:26 -0400

    factored out generate_moves method and cleaned up kngiht_moves method

--- lib/main.rb
+++ lib/main.rb
@@ -1,33 +1,36 @@
 # frozen_string_literal: true
 
 require_relative 'tree'
-require_relative 'node'
 require_relative 'board'
 require_relative 'knight'
 
 def knight_moves(starting_position, ending_position)
-  @board = Board.new
-  @moves_tree = Tree.new(starting_position)
   return [starting_position] if starting_position == ending_position
 
-  @board.positions[starting_position[0]][starting_position[1]] = 1
+  board = Board.new(8)
+  unless board.valid_position?(starting_position) &&
+           board.valid_position?(ending_position)
+    return puts 'Invalid coordinates'
+  end
+
+  moves_tree = Tree.new(starting_position)
+  board.visited(starting_position)
   queue = [] << starting_position
 
+  generate_moves(ending_position, board, moves_tree, queue)
+end
+
+def generate_moves(ending_position, board, tree, queue)
   until queue.empty?
     curr_move = queue.shift
 
     Knight.moves.each do |shift|
       next_move = [curr_move, shift].transpose.map(&:sum)
+      next unless board.valid_position?(next_move)
 
-      unless next_move[0].between?(0, @board.positions.size - 1) &&
-               next_move[1].between?(0, @board.positions.size - 1) &&
-               @board.positions[next_move[0]][next_move[1]].zero?
-        next
-      end
-
-      @board.positions[next_move[0]][next_move[1]] = 1
-      @moves_tree.insert(next_move, curr_move)
-      return @moves_tree.backtrack(next_move) if next_move == ending_position
+      board.visited(next_move)
+      tree.insert(next_move, curr_move)
+      return tree.backtrack(next_move) if next_move == ending_position
 
       queue << next_move
     end
